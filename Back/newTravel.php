560c05933c0727c0f8738445f6dfe55847eeb16b
Daniel <danielmonr@gmail.com>
2016-11-30 16:01:11 -0600

    landing

--- Back/newTravel.php
+++ Back/newTravel.php
@@ -5,7 +5,7 @@
 
    $res = $_POST['data'];
    //$decode = json_decode($res);
-   echo var_dump($res);
+   echo $res[0][0];
    /*$dest = $decode["destino"];
    $tipo = $decode["tipo"];
    $desc = $decode["descripcion"];
