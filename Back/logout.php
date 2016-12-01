186a3b0e678564f6446de3b59f5ed903652d4777
Daniel <danielmonr@gmail.com>
2016-11-30 21:22:11 -0600

    back end

--- Back/logout.php
+++ Back/logout.php
@@ -1,10 +1,7 @@
 <?php
-   include_once("DBConnection.php");
 
-   //print("entro<br/>");
-
-
-    $_SESSION['login_user'] = NULL;
-    header("location: http://ubiquitous.csf.itesm.mx/~daw-1021514/content/DAW_EXF/Front/index.html");
+   if(session_destroy()){
+       header("Location: http://ubiquitous.csf.itesm.mx/~daw-1021514/content/DAW_EXF/Front/index.html");
+   }
 
 ?>
